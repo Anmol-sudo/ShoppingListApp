d24fee7420f2efe678f8682efaf71093027b6a72
Anmol-Sudo <anmolshiv1974@gmail.com>
2025-06-19 15:02:25 +0530

    add lazy column and implement state management

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -6,16 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,41 +32,53 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text("Add Item")
-                        }
-                    }
+                    ShoppingListApp(innerPadding)
                 }
             }
         }
     }
 }
 
+@Composable
+fun ShoppingListApp(paddingValues: PaddingValues) {
+    // State to hold the list of shopping items
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(sItems){
+
+            }
+        }
+    }
+}
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+)
+
 @Preview(showBackground = true)
 @Composable
 fun ShoppingAppPreview(){
     ShoppingListAppTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = {},
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                Text("Add Item")
-            }
-        }
+        val custompadding = PaddingValues(16.dp)
+        ShoppingListApp(custompadding)
     }
 }

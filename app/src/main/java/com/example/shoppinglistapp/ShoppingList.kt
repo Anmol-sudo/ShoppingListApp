3c741f1aec7765d14dc3a455cd9fe19c15d10d68
Anmol-Sudo <anmolshiv1974@gmail.com>
2025-06-19 17:01:34 +0530

    add ShoppingListItem composable and update ShoppingListApp to display items

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -57,7 +61,7 @@ fun ShoppingListApp(paddingValues: PaddingValues) {
                 .padding(16.dp)
         ) {
             items(sItems){
-
+                ShoppingListItem(it, {}, {})
             }
         }
     }
@@ -112,5 +116,21 @@ fun ShoppingListApp(paddingValues: PaddingValues) {
                 }
             }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.padding(8.dp).fillMaxWidth().border(
+            border = BorderStroke(2.dp, Color(0XFF018786)),
+            shape = RoundedCornerShape(20)
+        )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }

6d41fa5025326d0de7bda9697d2630a9e200ac8e
Anmol-Sudo <anmolshiv1974@gmail.com>
2025-06-19 15:06:15 +0530

    optimize imports

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -4,25 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.lazy.items
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,46 +27,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun ShoppingListApp(paddingValues: PaddingValues) {
-    // State to hold the list of shopping items
-    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(
-            onClick = {},
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text("Add Item")
-        }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            items(sItems){
 
-            }
-        }
-    }
-}
 
-data class ShoppingItem(
-    val id: Int,
-    var name: String,
-    var quantity: Int,
-    var isEditing: Boolean = false
-)
 
 @Preview(showBackground = true)
 @Composable
 fun ShoppingAppPreview(){
     ShoppingListAppTheme {
-        val custompadding = PaddingValues(16.dp)
-        ShoppingListApp(custompadding)
+        val customPadding = PaddingValues(16.dp)
+        ShoppingListApp(customPadding)
     }
 }

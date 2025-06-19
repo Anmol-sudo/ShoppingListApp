e235aa41f57d2ebfbb71e8bc933f3e44330d7469
Anmol-Sudo <anmolshiv1974@gmail.com>
2025-06-19 15:31:31 +0530

    implement dialog for adding shopping items

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,10 +28,13 @@ data class ShoppingItem(
 )
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(paddingValues: PaddingValues) {
     // State to hold the list of shopping items
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,7 +42,7 @@ fun ShoppingListApp(paddingValues: PaddingValues) {
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = {},
+            onClick = {showDialog = true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text("Add Item")
@@ -50,6 +55,14 @@ fun ShoppingListApp(paddingValues: PaddingValues) {
             items(sItems){
 
             }
+        }
+    }
+
+    if (showDialog){
+        BasicAlertDialog(
+            onDismissRequest = {showDialog = false}
+        ) {
+            Text("I'M AN ALERT DIALOG!")
         }
     }
 }
